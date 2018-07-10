848c11417885eab50981e95d5d9ca5ee0bdcb224
hoadinhquoc <hoadinhquoc@gmail.com>
2018-07-10 23:54:14 +0700

    +Moving obstacle

---
+++ Assets/Scripts/Gameplay/MovePingPong.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePingPong : MonoBehaviour {
+
+	[SerializeField] float Speed = 2f;
+	[SerializeField] Vector3 StartPoint;
+	[SerializeField] Vector3 EndPoint;
+
+	float timer = 0f;
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+        float distance = (EndPoint - StartPoint).magnitude;
+        float t = Mathf.PingPong(timer * Speed, distance) / distance;
+
+        transform.position = Vector3.Lerp(StartPoint, EndPoint, t);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(StartPoint, EndPoint);
+    }
+
+    private void OnGUI()
+    {
+        Debug.Log("OnGUI");
+    }
+}
