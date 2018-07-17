2d191c87a3fd5b6c3dd3a341260fa2d66be166fd
hoadq <hoadinhquoc@gmail.com>
2018-07-17 11:35:45 +0700

    +UIManager

--- Assets/Scripts/Gameplay/ActionPhase.cs
+++ Assets/Scripts/Gameplay/ActionPhase.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class ActionPhase : MonoBehaviour {
 
-	void Awake()
-	{
-		GameEvents.START_GAME += OnGameStart;
-	}
 	public void OnChangeDirectionTap()
 	{
 		GameEvents.MC_CHANGE_DIRECTION.Raise();
-	}
-
-	void OnGameStart()
-	{
-		this.gameObject.SetActive(true);
 	}
 }
---
+++ Assets/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	[SerializeField] GameObject MenuMain;
+	[SerializeField] GameObject MenuAP;
+	[SerializeField] GameObject MenuResult;
+
+	void Awake()
+	{
+		GameEvents.START_GAME += OnGameStart;
+		GameEvents.GAME_OVER += OnGameOver;
+	}
+
+	void OnGameStart()
+	{
+		MenuMain.SetActive(false);
+		MenuAP.SetActive(true);
+	}
+
+	void OnGameOver()
+	{
+		MenuAP.SetActive(false);
+		MenuResult.SetActive(true);
+	}
+}
