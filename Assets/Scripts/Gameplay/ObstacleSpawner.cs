5f3434190b321fdebd535d9193d7e88390934af7
hoadq <hoadinhquoc@gmail.com>
2018-07-21 16:58:41 +0700

    *Fix spawner

--- Assets/Scripts/Gameplay/ObstacleSpawner.cs
+++ Assets/Scripts/Gameplay/ObstacleSpawner.cs
@@ -2,79 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour {
-	public static ObstacleSpawner Instance;
-	[SerializeField] Obstacle obstaclePrefab;
-	[SerializeField] MinMaxFloat SpawnYRange;
-	[SerializeField] float timeToSpawn;
-	int numberOfObstacle = 0;
-	List<Obstacle> childList;
-	StageData stageData;
-	// Use this for initialization
-	void Awake () {
-		Instance = this;
-		childList = new List<Obstacle>();
+public class ObstacleSpawner : MonoBehaviour
+{
+    public static ObstacleSpawner Instance;
+    [SerializeField] Obstacle obstaclePrefab;
+    [SerializeField] MinMaxFloat SpawnYRange;
+    [SerializeField] float timeToSpawn;
+    int numberOfObstacle = 0;
+    List<Obstacle> childList;
+    StageData stageData;
+    // Use this for initialization
+    void Awake()
+    {
+        Instance = this;
+        childList = new List<Obstacle>();
 
-		GameEvents.START_GAME += OnGameStart;
-		GameEvents.GAME_OVER += OnGameOver;
-		GameEvents.STAGE_CHANGED += OnStageChanged;
-	}
-	
-	// Update is called once per frame
-	void OnStageChanged () {
-		stageData = StageManager.Instance.CurrentStage;
-		numberOfObstacle = stageData.NumberOfObstacle;
-	}
+        GameEvents.START_GAME += OnGameStart;
+        GameEvents.GAME_OVER += OnGameOver;
+        GameEvents.STAGE_CHANGED += OnStageChanged;
+    }
 
-	void OnGameStart()
-	{
-		InvokeRepeating("SpawnChild", 0, timeToSpawn);
-	}
-	void OnGameOver()
-	{
-		CancelInvoke("SpawnChild");
-		for(int i = 0; i < childList.Count;i++)
-		{
-			Destroy(childList[i].gameObject);
-		}
+    // Update is called once per frame
+    void OnStageChanged()
+    {
+        stageData = StageManager.Instance.CurrentStage;
+        numberOfObstacle = stageData.NumberOfObstacle;
+    }
 
-		childList.Clear();
-	}
+    void OnGameStart()
+    {
+        InvokeRepeating("SpawnChild", 0, timeToSpawn);
+    }
+    void OnGameOver()
+    {
+        CancelInvoke("SpawnChild");
+        for (int i = 0; i < childList.Count; i++)
+        {
+            Destroy(childList[i].gameObject);
+        }
 
-	void SpawnChild()
-	{
-		if(childList.Count < numberOfObstacle)
-		{
-			int totalMissingOb = numberOfObstacle - childList.Count;
-			for(int i = 0 ; i < totalMissingOb; i ++)
-			{
-				childList.Add(Instantiate(obstaclePrefab.gameObject, transform).GetComponent<Obstacle>());
-			}
-		}
-		else if (childList.Count > numberOfObstacle)
-		{
-			int totalRemovedOb = childList.Count - numberOfObstacle;
-			for(int i = 0 ; i < totalRemovedOb ; i ++)
-			{
-				GameObject killedChild = childList[0].gameObject;
-				childList.RemoveAt(0);
-				Destroy(killedChild);
-			}
-		}
+        childList.Clear();
+    }
 
-		for(int i = 0; i < childList.Count; i ++)
-		{
-			Obstacle ob = childList[i];
+    void SpawnChild()
+    {
+        if (childList.Count < numberOfObstacle)
+        {
+            int totalMissingOb = numberOfObstacle - childList.Count;
 
-			ob.SetSpeed(stageData.ObstacleSpeed.RandomValue);
-			ob.SetPositionY(SpawnYRange.RandomValue);
-			ob.Run();
-		}
-	}
+            Obstacle ob = Instantiate(obstaclePrefab.gameObject, transform).GetComponent<Obstacle>();
 
-	public void OnChildRemoved(Obstacle ob)
-	{
-		childList.Remove(ob);
-		Destroy(ob.gameObject);
-	}
+            childList.Add(ob);
+
+            ob.SetSpeed(stageData.ObstacleSpeed.RandomValue);
+            ob.SetPositionY(SpawnYRange.RandomValue);
+            ob.Run();
+
+        }
+
+
+    }
+
+    public void OnChildRemoved(Obstacle ob)
+    {
+        childList.Remove(ob);
+        Destroy(ob.gameObject);
+    }
 }
