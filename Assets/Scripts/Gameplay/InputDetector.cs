7097c7854fc3f3b402059bcc59d62cacbafe1353
hoadq <hoadinhquoc@gmail.com>
2018-07-21 16:40:12 +0700

    *Fix touch

---
+++ Assets/Scripts/Gameplay/InputDetector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDetector : MonoBehaviour {
+
+	void Awake()
+	{
+		Input.simulateMouseWithTouches = true;
+	}
+	// Use this for initialization
+	enum State
+	{
+		RELEASE,
+		DOWN
+	}
+
+	State m_state = State.RELEASE;
+
+	bool IsState(State state)
+	{
+		return m_state == state;
+	}
+
+	void SetState(State state)
+	{
+		m_state = state;
+	}
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			if(IsState(State.RELEASE))
+			{
+				GameEvents.MC_CHANGE_DIRECTION.Raise();
+			}
+
+			SetState(State.DOWN);
+		}
+		else
+		{
+			SetState(State.RELEASE);
+		}
+	}
+}
