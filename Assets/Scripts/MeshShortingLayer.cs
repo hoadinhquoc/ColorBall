0397a480c175c387f5277c3c021fd95c51c88f6f
hoadinhquoc <hoadinhquoc@gmail.com>
2018-08-20 00:43:55 +0700

    +Score VFX

---
+++ Assets/Scripts/MeshShortingLayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshShortingLayer : MonoBehaviour {
+	[SerializeField] string SortingLayerName = "Default";
+	[SerializeField] int OrderInLayer = 0;
+	// Use this for initialization
+	MeshRenderer meshRenderer;
+	void Awake () {
+		meshRenderer = GetComponent<MeshRenderer>();
+	}
+
+	void Start()
+	{
+		UpdateSortingLayer();
+	}
+	
+	void UpdateSortingLayer()
+	{
+		meshRenderer.sortingLayerName = this.SortingLayerName;
+		meshRenderer.sortingOrder = OrderInLayer;
+	}
+
+	void OnValidate()
+	{
+		meshRenderer = GetComponent<MeshRenderer>();
+		UpdateSortingLayer();
+	}
+}
---
+++ Assets/Scripts/ScoreVFX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreVFX : MonoBehaviour {
+    MeshRenderer m_renderer;
+    TextMesh m_textMesh;
+	// Use this for initialization
+	void Awake () {
+        m_textMesh = GetComponent<TextMesh>();
+        m_renderer = GetComponent<MeshRenderer>();
+        m_renderer.enabled = false;
+        GameEvents.INSCREASE_SCORE += OnIncreaseScore;
+	}
+	
+	// Update is called once per frame
+	void OnIncreaseScore(int scoreAdd) {
+        m_textMesh.text = "+" + scoreAdd.ToString();
+        CancelInvoke("Hide");
+        Invoke("Hide", 0.5f);
+        m_renderer.enabled = true;
+    }
+
+    void Hide()
+    {
+        m_renderer.enabled = false;
+    }
+}
