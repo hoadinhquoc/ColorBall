75e3bfcc022dc2de2f78d41536fb8053a571cd38
hoadinhquoc <hoadinhquoc@gmail.com>
2018-08-20 00:23:25 +0700

    +Tutorial touch platform

---
+++ Assets/Scripts/Gameplay/StaticObstacle.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticObstacle : MonoBehaviour {
+	enum State{
+		HIDE,
+		SHOW_INDICATOR,
+		MOVING_IN,
+		STAND,
+		MOVING_OUT
+	}
+	[SerializeField] EventTrigger EventType;
+	[SerializeField] Transform TopLaser;
+	[SerializeField] GameObject TopCollider;
+	[SerializeField] Transform BottomLaser;
+	[SerializeField] GameObject BottomCollider; 
+	[SerializeField] float TimeToShowIndicator = 1f;
+	[SerializeField] float TimeStandBy = 2.5f;
+	[SerializeField] float Speed = 4f;
+	[SerializeField] float TopStartPositionY = 7f;
+	[SerializeField] float TopDestinationPositionY = 1f;
+	State m_state = State.HIDE;
+	float m_IndicatorTimer = 0f;
+	float m_StandTimer = 0f;
+
+	float m_TimeTriggerTimer = 0f;
+    int m_ScoreTrigger = 0;
+	
+	// Use this for initialization
+	void Awake()
+	{
+		GameEvents.GAME_OVER += Reset;
+		GameEvents.SCORE_CHANGED += OnScoreChanged;
+		Reset();
+	}
+	void Reset()
+	{
+		Hide();
+
+		m_TimeTriggerTimer = 0f;
+        m_ScoreTrigger = (int)EventType.StartValue;
+	}
+	void Hide()
+    {
+		TopLaser.gameObject.SetActive(false);
+		BottomLaser.gameObject.SetActive(false);
+		m_state = State.HIDE;
+		m_StandTimer = 0f;
+		m_IndicatorTimer = 0f;
+	}
+	void OnScoreChanged(int score)
+    {
+        if (EventType.Type == EventTriggerType.SCORE)
+        {
+            if (score >= m_ScoreTrigger)
+            {
+                Run();
+                m_ScoreTrigger += (int)EventType.RepeatValue;
+            }
+        }
+    }
+	void Run()
+	{
+		m_state = State.SHOW_INDICATOR;
+		TopLaser.transform.position = new Vector3(0f,TopStartPositionY, 0f);
+		BottomLaser.transform.position = new Vector3(0f, -TopStartPositionY, 0f); 
+		TopCollider.SetActive(false);
+		BottomCollider.SetActive(false);
+		TopLaser.gameObject.SetActive(true);
+		BottomLaser.gameObject.SetActive(true);
+		GameEvents.RUN_STATIC_OBSTACLE.Raise();
+	}
+	// Update is called once per frame
+	void Update () {
+		float dt = Time.deltaTime;
+		if(m_state == State.MOVING_IN)
+		{
+			Vector3 topLaserPos = TopLaser.position;
+			topLaserPos.y -= Speed * dt;
+			TopLaser.position = topLaserPos;
+
+			Vector3 bottomLaserPos = BottomLaser.position;
+			bottomLaserPos.y += Speed * dt;
+			BottomLaser.position = bottomLaserPos; 
+
+			if(topLaserPos.y < TopDestinationPositionY)
+				m_state = State.STAND;
+		}
+		else if(m_state == State.MOVING_OUT)
+		{
+			Vector3 topLaserPos = TopLaser.position;
+			topLaserPos.y += Speed * dt;
+			TopLaser.position = topLaserPos;
+
+			Vector3 bottomLaserPos = BottomLaser.position;
+			bottomLaserPos.y -= Speed * dt;
+			BottomLaser.position = bottomLaserPos;
+
+			if(topLaserPos.y > TopStartPositionY)
+			{
+				Hide();
+				GameEvents.END_STATIC_OBSTACLE.Raise();
+			}
+		}
+		else if(m_state == State.SHOW_INDICATOR)
+		{
+			m_IndicatorTimer += dt;
+			if(m_IndicatorTimer > TimeToShowIndicator)
+			{
+				TopCollider.SetActive(true);
+				BottomCollider.SetActive(true);
+				m_state = State.MOVING_IN;
+			}
+		}
+		else if(m_state == State.STAND)
+		{
+			m_StandTimer += dt;
+			if(m_StandTimer > TimeStandBy)
+				m_state = State.MOVING_OUT;
+		}
+		
+
+	}
+
+	void LateUpdate()
+    {
+        if (EventType.Type == EventTriggerType.TIME && m_state == State.HIDE)
+        {
+            m_TimeTriggerTimer += Time.deltaTime;
+
+            if (GameManager.Instance.SingleRunTime > EventType.StartValue)
+            {
+                if(m_TimeTriggerTimer > EventType.RepeatValue)
+                {
+                    m_TimeTriggerTimer = 0f;
+                    Run();
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    [System.Serializable]
+    class TutorialSave
+    {
+        public List<TUTORIAL_STEP> CompleteTutorialList;
+        public TutorialSave()
+        {
+            CompleteTutorialList = new List<TUTORIAL_STEP>();
+        }
+    }
+    TutorialSave m_data;
+    public static TutorialManager Instance;
+    void LoadSave()
+    {
+        string saveString = PlayerPrefs.GetString("Tutorial", string.Empty);
+
+        if (string.IsNullOrEmpty(saveString))
+        {
+            m_data = new TutorialSave();
+        }
+        else
+        {
+            m_data = JsonUtility.FromJson<TutorialSave>(saveString);
+        }
+    }
+    string GetSaveString()
+    {
+        return JsonUtility.ToJson(m_data);
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetString("Tutorial", GetSaveString());
+    }
+
+    public enum TUTORIAL_STEP
+    {
+        HIT_FLATFORM = 0,
+        COLOR_NODE,
+        COUNT
+    }
+
+    TUTORIAL_STEP m_currentTutorialStep = TUTORIAL_STEP.COUNT;
+    bool[] TutorialsFinish;
+
+    [SerializeField] GameObject TutorialHand;
+
+    void Awake()
+    {
+        Instance = this;
+        Init();
+        GameEvents.START_GAME += OnGameStart;
+    }
+    public void SetFinishTutorial(TUTORIAL_STEP step)
+    {
+        m_currentTutorialStep = TUTORIAL_STEP.COUNT;
+        m_data.CompleteTutorialList.Add(step);
+        UpdateTutorialStatus();
+        Save();
+    }
+
+    public bool IsFinishTutorial(TUTORIAL_STEP step)
+    {
+        return TutorialsFinish[(int)step];
+    }
+
+    public void ResetTutorial()
+    {
+        m_data.CompleteTutorialList.Clear();
+        UpdateTutorialStatus();
+
+        Save();
+    }
+
+    public void ResetTutorial(TUTORIAL_STEP step)
+    {
+        if (m_data.CompleteTutorialList.Exists(x => x == step))
+        {
+            m_data.CompleteTutorialList.Remove(step);
+            UpdateTutorialStatus();
+        }
+    }
+
+    void Init()
+    {
+        LoadSave();
+        TutorialsFinish = new bool[(int)TUTORIAL_STEP.COUNT];
+        UpdateTutorialStatus();
+    }
+
+    void UpdateTutorialStatus()
+    {
+        for (int i = 0; i < TutorialsFinish.Length; i++)
+        {
+            TutorialsFinish[i] = false;
+        }
+        for (int i = 0; i < m_data.CompleteTutorialList.Count; i++)
+        {
+            TutorialsFinish[(int)m_data.CompleteTutorialList[i]] = true;
+        }
+    }
+    void OnGameStart()
+    {
+        if(!IsFinishTutorial(TUTORIAL_STEP.HIT_FLATFORM))
+        {
+            Invoke("ShowTutorialHitPlatform", 1f);
+        }
+    }
+
+    //
+    void ShowTutorialHitPlatform()
+    {
+        Time.timeScale = 0.2f;
+        TutorialHand.SetActive(true);
+        GameEvents.MC_CHANGE_DIRECTION += HideTutotiralHitPlatform;
+    }
+    void HideTutotiralHitPlatform()
+    {
+        Time.timeScale = 1f;
+        TutorialHand.SetActive(false);
+        SetFinishTutorial(TUTORIAL_STEP.HIT_FLATFORM);
+        GameEvents.MC_CHANGE_DIRECTION -= HideTutotiralHitPlatform;
+    }
+}
