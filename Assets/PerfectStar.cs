544f573be55b3abc06cfd82aa8660cbba3dbcc50
hoadq <hoadinhquoc@gmail.com>
2018-08-20 18:53:35 +0700

    *Fix crash

--- Assets/PerfectStar.cs
+++ Assets/PerfectStar.cs
@@ -9,7 +9,10 @@ public class PerfectStar : MonoBehaviour {
         m_spriteRenderer = GetComponent<SpriteRenderer>();
         GameEvents.MC_CHANGED_COLOR += OnMCChangedColor;
     }
-	
+	void OnDestroy()
+    {
+        GameEvents.MC_CHANGED_COLOR -= OnMCChangedColor;
+    }
 	void OnMCChangedColor(Color color)
     {
         m_spriteRenderer.color = color;
